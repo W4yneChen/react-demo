864abcb4907a72243fb181231c0848a6184275a7
chenzhenhui.wayne <chenzhenhui.wayne@bytedance.com>
2022-07-18 19:57:22 +0800

    Add ArrayDiff & TextDiff

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import { useState } from 'react';
 import './App.css';
 
-function App() {
+function ArrayDiff() {
+  const [state, setState] = useState([1, 2, 3]);
+  const handleClick = () => {
+    const a = state[1];
+    const b = state[2];
+    setState([1, b, a]);
+  };
+
+  return (
+    <div onClick={handleClick}>
+      { state.map(v => (<div key={v}>{v}</div>)) }
+    </div>
+  );
+}
+
+function TextDiff() {
   const [state, setState] = useState(0);
-  const handleClick = () => setState(v => ++v);
+  const handleClick = () => {
+    setState(c => ++c);
+  };
 
   return (
     <div onClick={handleClick}>{state}</div>
   );
+}
+
+function App() {
+  return (
+    // <ArrayDiff />
+    <TextDiff />
+  )
 }
 
 export default App;
